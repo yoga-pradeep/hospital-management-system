617b2265f015cb8ee76150d39c9f11421c429a62
yoga-pradeep <yogapradeep999@gmail.com>
2026-09-23 12:53:52 +0530

    Move mail credentials to environment variables

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask, send_from_directory
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
--- backend/mail_config.py
+++ backend/mail_config.py
@@ -1,12 +1,13 @@
 from flask_mail import Mail
+import os
 
 mail = Mail()
 
 def init_mail(app):
-    app.config['MAIL_SERVER']         = 'smtp.gmail.com'
-    app.config['MAIL_PORT']           = 587
+    app.config['MAIL_SERVER']         = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    app.config['MAIL_PORT']           = int(os.environ.get('MAIL_PORT', 587))
     app.config['MAIL_USE_TLS']        = True
-    app.config['MAIL_USERNAME']       = 'yogapradeep999@gmail.com'  
-    app.config['MAIL_PASSWORD']       = 'lfuc ehuc cxvo yotm'  
-    app.config['MAIL_DEFAULT_SENDER'] = 'yogapradeep999@gmail.com'
+    app.config['MAIL_USERNAME']       = os.environ.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD']       = os.environ.get('MAIL_PASSWORD')
+    app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
     mail.init_app(app)
